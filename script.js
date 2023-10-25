430cab2f566741f7e788709784a48c399adab50e
tataosmonalieva <emusya1010@mail.ru>
2023-10-25 17:13:20 +0600

    js

---
+++ script.js
@@ -0,0 +1,40 @@
+//boolean = true/false
+//string = "/"
+//number = 1 
+// undefined
+// null = 
+// symbol = 
+// bigint = 
+
+//let a = 4
+//let b = 55
+//console.log( a + b );
+
+//let a = 4
+//let b = 55
+//console.log (a * b);
+
+//let a = 55
+//let b = 4
+//console.log ( a / b );
+
+//let a = 55
+//let b = 4
+//console.log(a % b );
+
+//let a = 'hello'
+//let b = ' tanya'
+//console.log (a + b);
+
+//let a = 'hello'
+//let b = 'tanya'
+//console.log (a - b);
+
+//let a = 5
+//let b = '5'
+//b= 5
+//console.log(a + b);
+
+let c = 2
+let b = undefined
+    console.log(c + b);
